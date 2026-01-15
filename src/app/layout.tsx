3a4720b095129cae06335cef44a9fa70fb787821
thandepzai <cuthan2k1@gmail.com>
2026-01-16 00:39:14 +0700

    add some

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,23 @@
 import MainLayout from "module/_core/app/component/Layout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    icons: {
+        icon: "/images/logo-steering.svg",
+        apple: "/images/logo-steering.svg"
+    }
+};
 
 export default function RootLayout({
-  children,
+    children
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html>
-      <body>
-        <MainLayout>{children}</MainLayout>
-      </body>
-    </html>
-  );
+    return (
+        <html>
+            <body>
+                <MainLayout>{children}</MainLayout>
+            </body>
+        </html>
+    );
 }
